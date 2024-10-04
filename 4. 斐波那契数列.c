e6e5aa69ea318f79e393e0297c35551961137282
Rook-yu <244148798@qq.com>
2024-10-04 07:32:46 -0700

    Add files via upload

--- 4. 斐波那契数列.c
+++ 4. 斐波那契数列.c
@@ -1,12 +1,12 @@
 # include<stdio.h>
 
-void    feibo(int  t)
+void    feibo(int  t)   //计算斐波那契数列并输出
 {
     int n;
     int a1=1;
     int a2=1;
     int a3;
-    if (t==1|t==2)
+    if (t==1|t==2)           //1，2个特殊考虑
         printf("1\n");
     else 
         {
@@ -25,13 +25,13 @@ int main()
 {
     int m,i;
     scanf ("%d",&m);
-    int  a[m-1];
+    int  a[m-1];                    //建立数组存放数据
     for (i=0;i<=m-1;i++ )
         {
-        scanf ("%d",&a[i]);
+        scanf ("%d",&a[i]);       //数据入数组
         }
     i=0;
-    for (i=0;i<=m-1;i++ )
+    for (i=0;i<=m-1;i++ )       //读取数据并计算输出
         {
             feibo(a[i]);
 
